f613dd4480cb30ca7719e4d6ab3cf68052fd9376
manuel deneu <manuel.deneu@gmail.com>
2026-03-26 18:17:32 +0100

    app: render test img

--- App/app/renderer.py
+++ App/app/renderer.py
@@ -7,6 +7,7 @@ SOURCE_RESOLUTION = (3840, 160)
 class Renderer:
     def __init__(self):
         self.surface = pygame.Surface(SOURCE_RESOLUTION)
+        self.test_surface = pygame.image.load("test.png")
 
     def update(self):
-        pass
+        self.surface.blit(self.test_surface, dest=(0, 0))
