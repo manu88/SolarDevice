e31f8607f33095ad87aafdf8b2cad129a7145b49
manuel deneu <manuel.deneu@gmail.com>
2026-03-26 18:31:10 +0100

    app: render basic animation

--- App/app/renderer.py
+++ App/app/renderer.py
@@ -8,6 +8,16 @@ class Renderer:
     def __init__(self):
         self.surface = pygame.Surface(SOURCE_RESOLUTION)
         self.test_surface = pygame.image.load("test.png")
+        self.offset_inc = 50
+        self.x_offset = 0
 
     def update(self):
-        self.surface.blit(self.test_surface, dest=(0, 0))
+        overflow = self.test_surface.get_width() - self.x_offset
+
+        self.surface.blit(self.test_surface, dest=(self.x_offset, 0))
+        self.surface.blit(self.test_surface, dest=(0, 0), area=(
+            overflow, 0, self.x_offset, SOURCE_RESOLUTION[1]))
+
+        self.x_offset += self.offset_inc
+        if self.x_offset >= self.test_surface.get_width():
+            self.x_offset = 0
