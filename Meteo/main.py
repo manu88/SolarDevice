4838524fb3d1af38d09ece6b0768110d93e0463c
manuel deneu <manuel.deneu@gmail.com>
2026-06-02 10:27:54 +0200

    Meteo: project cleanup

--- Meteo/main.py
+++ Meteo/main.py
@@ -1,20 +1,9 @@
 import argparse
 import sys
 from infoclimat.api import API
-from display.mire import gen_mire
-from app.app import App
 
 
-parser = argparse.ArgumentParser(prog='SolarDevice')
-parser.add_argument('-m', '--mire',
-                    action='store_true')
-parser.add_argument('-a', '--api',
-                    action='store_true')
-
-parser.add_argument('--show-source',
-                    action='store_true')
-
-parser.add_argument('-c', '--conf')
+parser = argparse.ArgumentParser(prog='Meteo')
 
 
 def test_api():
@@ -27,18 +16,7 @@ def test_api():
         f"nebu: {weather_entry.current_nebulosite()} -> {weather_entry.next_nebulosite()}")
 
 
-def run_app(args):
-    conf_file_path = args.conf if args.conf else ""
-    app = App(conf_file_path, show_source=args.show_source)
-    app.run()
-
-
 if __name__ == "__main__":
     args = parser.parse_args()
-    if args.api:
-        test_api()
-        sys.exit(0)
-    if args.mire:
-        gen_mire()
-        sys.exit(0)
-    run_app(args)
+    test_api()
+    sys.exit(0)
