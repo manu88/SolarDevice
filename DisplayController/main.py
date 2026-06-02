2827b3b1e5e971f1bc191301f7f9c2d1418b0737
manu88 <manuel.deneu@gmail.com>
2026-06-02 08:35:31 +0100

    DisplayController: refactoring

--- DisplayController/main.py
+++ DisplayController/main.py
@@ -1,14 +1,6 @@
-import sys
-import glob
-import serial
 import argparse
-import struct
-import serial
-from serial import serialutil
-import time
-from pythonosc.dispatcher import Dispatcher
-from pythonosc import osc_server
 from utils import serial_ports
+from controller import Controller
 
 
 def list_serial_ports():
@@ -16,68 +8,12 @@ def list_serial_ports():
     for p in ports:
         print(p)
 
-payload_size = 72
-
-
-class Controller:
-    def __init__(self):
-        self.arduino = serial.Serial(port='/dev/cu.usbmodem1401',
-                                     baudrate=115200, timeout=.1)
-
-        self.pack_com_str = ">BB"
-
-        self.payload = [0 for i in range(payload_size)]
-
-        self.dispatcher = Dispatcher()
-        self.dispatcher.map("/pix", self.osc_set_pix)
-        self.dispatcher.map("/all", self.osc_set_all)
-        self.dispatcher.map("/update", self.osc_update)
-        self.server = osc_server.ThreadingOSCUDPServer(
-            ("127.0.0.1", 8010), self.dispatcher)
-
-    def set_pix(self,  i: int, r: int, g: int, b: int):
-        self.payload[i*3] = r
-        self.payload[(i*3)+1] = g
-        self.payload[(i*3)+2] = b
-        # self.send_payload(self.payload)
-
-    def osc_update(self, args):
-        self.send_payload(self.payload)
-
-    def osc_set_all(self, args, r: float, g: float, b: float):
-        for i in range(72//3):
-            self.payload[i*3] = int(r)
-            self.payload[(i*3)+1] = int(g)
-            self.payload[(i*3)+2] = int(b)
-        # self.send_payload(self.payload)
-
-    def osc_set_pix(self, args, i: int, r: float, g: float, b: float):
-        if i*3 >= len(self.payload):
-            return
-        self.set_pix(i, int(r), int(g), int(b))
-
-    def send_payload(self, payload: list):
-        data_header = struct.pack(self.pack_com_str, 0XAF, len(payload))
-        try:
-            self.arduino.write(data_header)
-            self.arduino.write(payload)
-        except serialutil.SerialException as e:
-            print(f"send_payload:SerialException {e}")
-            self.stop()
-
-    def start(self):
-        print("Serving on {}".format(self.server.server_address))
-        self.server.serve_forever()
-
-    def stop(self):
-        print("Stopping")
-        self.server.shutdown()
-        self.arduino.close()
-
 
 parser = argparse.ArgumentParser(
-                    prog='DisplayController')
-parser.add_argument("-l", "--list", action="store_true")
+    prog='DisplayController')
+parser.add_argument(
+    "-l", "--list", help="list serial ports and exit", action="store_true")
+
 
 def main():
     args = parser.parse_args()
