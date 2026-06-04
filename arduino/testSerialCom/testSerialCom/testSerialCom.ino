12660cc3f9384c42ea2cc1b7a85d6661fe58ab94
manuel deneu <manuel.deneu@gmail.com>
2026-06-04 18:32:01 +0200

    arduino: add sensor reading code to testSerialCom

--- arduino/testSerialCom/testSerialCom/testSerialCom.ino
+++ arduino/testSerialCom/testSerialCom/testSerialCom.ino
@@ -1,6 +1,25 @@
 #include "proto.hpp"
 #include <FastLED.h>
 
+
+
+/////////////////////////////////
+const int numReadings = 50;
+int readings[numReadings];  // the readings from the analog input
+int readIndex = 0;          // the index of the current reading
+int total = 0;              // the running total
+int average = 0;            // the average
+
+const int minPeakDiff = 20;
+
+unsigned long revStartTime = 0;
+unsigned long lastIdleCheckTime = 0;
+const unsigned long IdleInterval = 5000;
+
+int inputPin = A0;
+int inPeak = 0;
+/////////////////////////////////
+
 #define NUM_LEDS 24
 #define DATA_PIN 3 // Change this to match your LED strip's data pin
 #define CLOCK_PIN 13
@@ -22,6 +41,8 @@ void setup() {
 
   FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
+
+  resetReadings();
 
   setAll(255, 255, 255);
 
@@ -141,6 +162,62 @@ void loop() {
       resetParserState();
     }
   }
-
+  loopSensor();
   // delay(10);
+}
+
+void loopSensor() {
+  total = total - readings[readIndex];
+
+  int val = analogRead(inputPin);
+  readings[readIndex] = val;
+
+  total = total + readings[readIndex];
+  readIndex = readIndex + 1;
+
+  
+  if (readIndex >= numReadings) {
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+
+  unsigned long now = millis();
+  int reading = val > average + minPeakDiff;
+  
+  if (reading ){
+    lastIdleCheckTime = now;
+    if(inPeak == 0){
+      float speed = -1;
+      if (revStartTime > 0){
+        unsigned long ellapsed = now - revStartTime;
+        speed = 1000.f/ellapsed;
+      }
+      sendStatus(speed, reading);
+      revStartTime = now;
+    }
+    inPeak = 1;
+  }else{
+    inPeak = 0;
+  }
+  if (now - lastIdleCheckTime >IdleInterval){
+    resetReadings();
+    sendStatus(0, 0);
+    lastIdleCheckTime = now;
+  }
+  delay(2);
+}
+
+void resetReadings(){
+  for (int i = 0; i < i; i++) {
+    readings[i] = 0;
+  }
+}
+
+void sendStatus(float speed, int activity){
+  Serial.print(speed);
+  Serial.print(" ");
+  Serial.print(activity);
+  Serial.print(";\n");
 }
