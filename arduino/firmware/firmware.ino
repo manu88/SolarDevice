1d1f9c1c1b8d2b63f58fdad7aaadb620f502d42f
manuel deneu <manuel.deneu@gmail.com>
2026-06-15 09:48:33 +0200

    arduino: bump firmware to v0.0.2

--- arduino/firmware/firmware.ino
+++ arduino/firmware/firmware.ino
@@ -1,7 +1,7 @@
 #include "proto.hpp"
 #include <FastLED.h>
 
-#define FIRMWARE_VERSION "0.0.1"
+#define FIRMWARE_VERSION "0.0.2"
 
 size_t numSensorsIterations = 0;
 size_t timeSpentReadingSensors = 0;
