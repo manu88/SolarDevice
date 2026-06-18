adced2bf75401952da81672aa11b9955cd1a12e4
manuel deneu <manuel.deneu@gmail.com>
2026-06-18 16:36:47 +0200

    arduino: update stripTester

--- arduino/stripTester/stripTester.ino
+++ arduino/stripTester/stripTester.ino
@@ -1,7 +1,7 @@
 #include <FastLED.h>
 
 
-#define NUM_LEDS 24
+#define NUM_LEDS 27
 #define DATA_PIN 3 // Change this to match your LED strip's data pin
 #define CLOCK_PIN 13
 #define BRIGHTNESS 255
@@ -9,11 +9,15 @@
 
 CRGB leds[NUM_LEDS];
 
+void set(int x, int r,int g,int b) {
+  leds[x] = CRGB(r, g, b);
+}
+
 void setAll(int r,int g,int b) {
   for (int x = 0; x < NUM_LEDS; x++) {
     leds[x] = CRGB(r, g, b);
   }
-  FastLED.show();
+  
 }
 
 void setup() {
@@ -21,14 +25,36 @@ void setup() {
   FastLED.setBrightness(BRIGHTNESS);
 }
 
+void chenillard(int x){
+  setAll(0,0,0);
+  set(x, 255,255,255);
+}
 
-void loop() {
+int index = 0;
+void loop(){
+  chenillard(index);
+  index++;
+  if (index>=NUM_LEDS){
+    index = 0;
+  }
+  FastLED.show();
+  delay(1000);
+}
+
+void loop2() {
   setAll(255,255,255);
+  FastLED.show();
   delay(2000);
-  setAll(255,0,0);
+
+  setAll(0,0,0);
+  FastLED.show();  
   delay(2000);
+  return;
   setAll(0,255,0);
-  delay(2000);
+  FastLED.show();
+  delay(2000);  
+
   setAll(0,0,255);
+  FastLED.show();
   delay(2000);
 }
