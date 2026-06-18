de25bb69704eb41ce8ff964b147ec180c31ed0ac
manuel deneu <manuel.deneu@gmail.com>
2026-06-18 10:09:03 +0200

    Scheduler: get osc dest address from CLI

--- Scheduler/README.md
+++ Scheduler/README.md
@@ -7,4 +7,6 @@ Gère la logique globale du système.
 ```bash
 #important: doit être lancé depuis le répertoire racine du repo, i.e, le répertoire parent.
 python3 -m Meteo.main config/config-test.json
+# spécifier addresse de destination des messages OSC, 127.0.0.1 par défaut.
+python3 -m Meteo.main --addr 192.168.1.2 config/config-test.json
 ```
--- Scheduler/main.py
+++ Scheduler/main.py
@@ -6,12 +6,15 @@ from config.Config import Config
 
 parser = argparse.ArgumentParser(prog='Scheduler')
 parser.add_argument("config", help="json-config path")
+parser.add_argument(
+    "--addr", help="network address to send msgs to", default="127.0.0.1")
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    print(f"sending msgs to '{args.addr}'")
     conf = Config.from_json_file(args.config)
     if not conf.is_valid:
         print(f"invalid config at '{args.config}'")
         sys.exit(1)
-    c = Controller("192.168.1.255", conf=conf)
+    c = Controller(args.addr, conf=conf)
     c.run()
