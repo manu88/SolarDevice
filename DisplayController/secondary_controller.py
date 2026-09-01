5f2b600aa32605168e59375c9fcc365da8398c6f
manuel deneu <manuel.deneu@gmail.com>
2026-09-01 17:21:49 +0200

    DisplayController: add secondary_controller

---
+++ DisplayController/secondary_controller.py
@@ -0,0 +1,69 @@
+import serial
+from typing import Dict
+import traceback
+import time
+
+
+class SecondaryController:
+    def __init__(self, serial_ports: list[str]) -> None:
+        self._should_stop = False
+
+        self.arduinos: Dict[str, serial.Serial] = dict()
+        for ports in serial_ports:
+            arduino = serial.Serial(
+                port=ports, baudrate=9600, timeout=.1)
+            self.arduinos[ports] = arduino
+        self.board_ids: Dict[int, serial.Serial] = dict()
+
+    def start(self):
+        while self._should_stop is False:
+            for arduino in self.arduinos.values():
+                self._check_arduino(arduino)
+
+    def send_motor(self, board_id: int, motor_id: int, duration: int):
+        if board_id not in self.board_ids:
+            return
+        arduino = self.board_ids[board_id]
+        arduino.write(f"{motor_id+1};{duration}")
+
+    def _process_arduino_msg(self, arduino: serial.Serial, l: str):
+        line = l.strip()
+        print(f"received '{line}'")
+        if line.startswith("BoardId="):
+            board_id = int(line[8:])
+            print(f"Board id is {board_id}")
+            self.board_ids[board_id] = arduino
+            print(f"setting arduino at {arduino.port} as board id {board_id}")
+
+    def read_arduino_msg(self, arduino: serial.Serial) -> bool:
+        ret = False
+        if arduino.in_waiting:
+            lines = arduino.readlines()
+            if len(lines) > 0:
+                ret = True
+            if len(lines):
+                for l in lines:
+                    try:
+                        self._process_arduino_msg(arduino, l.decode())
+                    except Exception as err:
+                        print(err)
+                        print(traceback.format_exc())
+
+        return ret
+
+    def _check_arduino(self, arduino: serial.Serial):
+        received_nothing_count = 0
+        while self._should_stop is False:
+            received_something = self.read_arduino_msg(arduino)
+            if received_something:
+                received_nothing_count = 0
+            else:
+                received_nothing_count += 1
+                if received_nothing_count > 3:
+                    print("-> Reopen arduino")
+                    # with self.arduino_lock:
+                    #    self.arduino.close()
+                    #    self._open_arduino()
+                    print("<- Done Reopen arduino")
+
+            time.sleep(1)
