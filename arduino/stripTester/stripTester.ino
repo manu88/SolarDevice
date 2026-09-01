4941b1bc974e2880ea145b10dc69996d7c599636
manuel deneu <manuel.deneu@gmail.com>
2026-09-01 16:39:35 +0200

    arduino: minor cleanup

--- arduino/stripTester/stripTester.ino
+++ arduino/stripTester/stripTester.ino
@@ -1,79 +1,50 @@
 #include <FastLED.h>
 
+#if (FASTLED_VERSION != 3001000)
+#error ("Invalid Fastled version, expected 3001000")
+#endif
 // Fastled version 3.1.0
-#define NUM_LEDS 26
-#define DATA_PIN 11// Change this to match your LED strip's data pin
+#define NUM_LEDS 12
+#define DATA_PIN 11
 #define CLOCK_PIN 13
 #define BRIGHTNESS 255
 
-
 CRGB leds[NUM_LEDS];
 
-void set(int x, int r,int g,int b) {
-  leds[x] = CRGB(r, g, b);
-}
+void set(int x, int r, int g, int b) { leds[x] = CRGB(r, g, b); }
 
-void setAll(int r,int g,int b) {
+void setAll(int r, int g, int b) {
   for (int x = 0; x < NUM_LEDS; x++) {
     leds[x] = CRGB(r, g, b);
   }
-  
 }
 
 void setup() {
+  Serial.begin(9600);
   FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
 }
 
-void chenillard(int x){
-  setAll(0,0,0);
-  set(x,x %2==0?255:0 ,0,x %2==0?0:255);
-}
-
-static int index_ = 0;
-void loop1(){
-  chenillard(index_);
-  index_++;
-  if (index_>=NUM_LEDS){
-    index_ = 0;
-  }
-  FastLED.show();
-  delay(100);
-}
-
 void loop() {
-  setAll(255,255,255);
+
+  Serial.println("White");
+  setAll(255, 255, 255);
   FastLED.show();
   delay(2000);
 
-  setAll(255,0,0);
-  FastLED.show();  
+  Serial.println("Red");
+  setAll(255, 0, 0);
+  FastLED.show();
   delay(2000);
 
-  setAll(0,255,0);
+  Serial.println("GReen");
+  setAll(0, 255, 0);
   FastLED.show();
-  delay(2000);  
 
-  setAll(0,0,255);
+  delay(2000);
+
+  Serial.println("Blue");
+  setAll(0, 0, 255);
   FastLED.show();
   delay(2000);
 }
-
-int inten = 0;
-int dir = 1;
-
-void loop34() {
-  setAll(inten,0,0);
-  FastLED.show();
-  FastLED.delay(40);
-
-  inten += dir*10;
-  if(inten >255){
-    dir = -1;
-    inten = 255;
-  }else if(inten <=0){
-    dir = 1;
-    inten = 0;
-  }
-}
-  
