67c0fa6bca98e19a6a61fef8e12519338a96c517
manuel deneu <manuel.deneu@gmail.com>
2026-09-02 12:08:07 +0200

    DisplayController: remove logs

--- DisplayController/osc_server.py
+++ DisplayController/osc_server.py
@@ -26,28 +26,28 @@ class OSCServer(OSCServerInterface):
     def osc_ping(self, args):
         print(f"ping {args}")
 
-    def osc_update(self, args):
-        print("Update")
+    def osc_update(self, _):
+        # print("osc_update")
         self.secondary_ctlr.display_ctrl.update_display()
 
-    def osc_clear1(self, args):
-        print("clear1")
+    def osc_clear1(self, _):
+        # print("clear1")
         self.secondary_ctlr.display_ctrl.clear_buffer()
 
-    def osc_set_all(self, args, r: float, g: float, b: float):
-        print("set_all")
+    def osc_set_all(self, _, r: float, g: float, b: float):
+        # print("set_all")
         self.secondary_ctlr.display_ctrl.set_all(int(r), int(g), int(b))
 
-    def osc_set_pix1(self, args, i: int, r: float, g: float, b: float):
-        print("set_pix1")
+    def osc_set_pix1(self, _, i: int, r: float, g: float, b: float):
+        # print("set_pix1")
         self.secondary_ctlr.display_ctrl.set_pix1(i, int(r), int(g), int(b))
 
     def osc_servo(self, _, servo_idx: int, duration_ms: int):
-        print(f"osc_servo servo_idx={servo_idx} duration_ms={duration_ms}")
+        # print(f"osc_servo servo_idx={servo_idx} duration_ms={duration_ms}")
         real_servo_idx = servo_idx % 3
         board_id = servo_idx//3
-        print(
-            f"Sending to board={board_id} real_servo_idx={real_servo_idx} duration={duration_ms}")
+        # print(
+        #    f"Sending to board={board_id} real_servo_idx={real_servo_idx} duration={duration_ms}")
         self.secondary_ctlr.send_motor(board_id, real_servo_idx, duration_ms)
 
     def start(self):
