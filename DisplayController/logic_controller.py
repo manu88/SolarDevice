b7e6e211a2bde2c3f73845d27d0cb4c6af95f610
manuel deneu <manuel.deneu@gmail.com>
2026-09-09 12:46:29 +0200

    DisplayController: limit motor spread to 1

--- DisplayController/logic_controller.py
+++ DisplayController/logic_controller.py
@@ -270,7 +270,7 @@ class LogicController:
         print(f"set_spread {level} -> spread= {spread}")
         self.pulse_anim.set_size(spread)
         if self.anim_state == AnimState.PULSES:
-            self.spread_motors = int(level*5)
+            self.spread_motors = min(int(level*5), 1)
             print(f"set self.spread_motors={self.spread_motors}")
             self._update_motors_list_to_check()
 
